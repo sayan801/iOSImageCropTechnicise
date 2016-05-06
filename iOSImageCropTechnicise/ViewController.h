4aba9659225a2541d95f6518656edf5a02ecdfe1
cribello <chandra.sengupta@mycuratio.com>
2016-05-06 09:52:23 +0530

    adding xcode project

---
+++ iOSImageCropTechnicise/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSImageCropTechnicise
+//
+//  Created by chandra shekhar sengupta on 06/05/16.
+//  Copyright © 2016 chandra shekhar sengupta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOSImageCropTechniciseTests/iOSImageCropTechniciseTests.m
@@ -0,0 +1,39 @@
+//
+//  iOSImageCropTechniciseTests.m
+//  iOSImageCropTechniciseTests
+//
+//  Created by chandra shekhar sengupta on 06/05/16.
+//  Copyright © 2016 chandra shekhar sengupta. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iOSImageCropTechniciseTests : XCTestCase
+
+@end
+
+@implementation iOSImageCropTechniciseTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
